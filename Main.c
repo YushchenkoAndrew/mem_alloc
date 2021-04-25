83f6d1bd3cdd3b8e5281c4a72a1a4c81c4b51c87
YushchenkoAndrew <Andrew10122000@gmail.com>
2021-04-25 16:26:59 +0300

    Change memory testing

--- Main.c
+++ Main.c
@@ -1,10 +1,10 @@
 #include "mem_alloc.h"
 
-#define SMALL_SIZE_MIN 0
+#define SMALL_SIZE_MIN 16
 #define SMALL_SIZE_MAX 64
 #define LARGE_SIZE_MIN 512
 #define LARGE_SIZE_MAX 1000000
-#define ITERATION_COUNT 500
+#define ITERATION_COUNT 200
 #define N 100
 #define RAND_RANGE(a, b) ((a) + rand() % ((b) - (a)))
 #define RAND_SIZE() \
@@ -16,83 +16,30 @@
 static unsigned int seed;
 
 typedef struct result {
-    unsigned long *ptr;
+    unsigned char *ptr;
     size_t size;
     unsigned long checksum;
 } result;
 
+unsigned long checksum(unsigned char *ptr, size_t sz) {
+    unsigned long chk = 0u;
+    while (--sz != 0)
+        chk -= *ptr++;
+    return chk;
+}
+
+void fill(unsigned char *ptr, size_t sz) {
+    size_t sz_ = sz;
+    while (sz >= sizeof(size_t)) {
+        ptr += sizeof(size_t);
+        sz -= sizeof(size_t);
+        *ptr = sz_;
+    }
+}
+
 
 int main() {
-    // result res[N] = {{ .ptr = NULL, .size = 0, .checksum = 0 }};
-
-    // /* I can haz randomness? */
-    // seed = (unsigned int)time(NULL);
-    // srand(seed);
-
-    // size_t prev = 0;
-
-    // /* malloc some randomly sized blocks */
-    // for (int i = 0; i < ITERATION_COUNT; ++i) {
-    //     int j = rand() % N;
-    //     if (res[j].ptr == NULL) {
-    //         size_t sz = RAND_SIZE();
-    //         res[j].ptr = mem_alloc(sz);
-    //         if (res[j].ptr != NULL) {
-    //             mem_show(res[j].ptr);
-    //             res[j].size = sz;
-    //             res[j].ptr[0] = prev;
-    //             res[j].checksum = (size_t)res[j].ptr[0] + sz;
-    //             prev += res[j].size;
-    //         }
-    //     } else {
-    //         if (res[j].checksum != (res[j].ptr[0] + res[j].size)) {
-    //             printf("Checksum failed");
-    //             return -1;
-    //         }
-
-    //         if (rand() & 1) {
-    //             size_t sz = RAND_SIZE();
-    //             void *x = mem_realloc(res[j].ptr, sz);
-
-    //             if (sz != 0 || x != NULL) {
-    //                 res[j].ptr = x;
-    //                 res[j].ptr[0] = prev;
-    //                 res[j].size = sz;
-    //                 res[j].checksum = (size_t)res[j].ptr[0] + sz;
-    //                 prev += res[j].size;
-    //             } else {
-    //                 res[j].ptr = NULL;
-    //                 res[j].size = 0;
-    //                 res[j].checksum = 0u;
-    //             }
-    //         } else {
-    //             mem_free(res[j].ptr);
-    //             res[j].ptr = NULL;
-    //             res[j].size = 0;
-    //             res[j].checksum = 0u;
-    //         }
-
-    //         mem_show(res[j].ptr);
-    //     }
-    // }
-
-    // /* Clean up our mess */
-    // for (int i = 0; i < N; ++i) {
-    //     if (res[i].ptr && res[i].checksum != (res[i].ptr[0] + res[i].size)) {
-    //         printf("Checksum failed");
-    //         return -1;
-    //     }
-    //     _free(res[i].ptr, (size_t) res[i].size);
-    // }
-
-    /* Stuff to store our checks */
-    char *ptrs[ITERATION_COUNT];
-    int sizes[ITERATION_COUNT];
-    char chrs[ITERATION_COUNT];
-
-    unsigned long total = 0u;
-    unsigned ctrSum = 0u;
-    unsigned testSum = 0u;
+    result res[N] = {{ .ptr = NULL, .size = 0, .checksum = 0 }};
 
     /* I can haz randomness? */
     seed = (unsigned int)time(NULL);
@@ -100,77 +47,50 @@ int main() {
 
     /* malloc some randomly sized blocks */
     for (int i = 0; i < ITERATION_COUNT; ++i) {
-        size_t sz = RAND_SIZE();
-        sizes[i] = 0;
-        ptrs[i] = mem_alloc(sz);
-        if (ptrs[i] != NULL) {
-            total += sz;
-            mem_show(ptrs[i]);
-            sizes[i] = sz;
-            char rchr = (char)RAND_RANGE(0, 256);
-            chrs[i] = rchr;
-            ctrSum += rchr;
-            for (size_t j = 0; j < sz; ++j) {
-                ptrs[i][j] = chrs[i];
+        int j = rand() % N;
+        if (res[j].ptr == NULL) {
+            size_t sz = RAND_SIZE();
+            res[j].ptr = mem_alloc(sz);
+            if (res[j].ptr != NULL) {
+                mem_show(res[j].ptr);
+                res[j].size = sz;
+                fill(res[j].ptr, sz);
+                res[j].checksum = checksum(res[j].ptr, sz);
             }
-        }
-    }
-
-    printf("%s - %7d\n\n", "Total", total);
-
-    /* Check on data modification */
-    for (int i = 0; i < ITERATION_COUNT; ++i) {
-        testSum += chrs[i];
-    }
-
-    // if (testSum != ctrSum) {
-    //     printf("ERROR Data modification");
-    //     return -1;
-    // }
-
-    /* free some of the pointers */
-    for (int i = 0; i < ITERATION_COUNT / 2; ++i) {
-        int index = rand() % ITERATION_COUNT;
-        mem_free(ptrs[index]);
-        mem_show(ptrs[index]);
-        ptrs[index] = NULL;
-        sizes[index] = 0;
-        chrs[index] = '\0';
-    }
-
-
-    /* realloc some of the pointers */
-    for (int i = 0; i < ITERATION_COUNT / 2; ++i) {
-        int index = rand() % ITERATION_COUNT;
-        size_t sz = RAND_SIZE();
-        void *x = mem_realloc(ptrs[index], sz);
-
-        if (sz == 0 || x != NULL) {
-            ptrs[index] = x;
-            mem_show(ptrs[index]);
-            sizes[index] = sz;
-            chrs[index] = (char)RAND_RANGE(0, 256);
-            for (size_t j = 0; j < sz; ++j) {
-                ptrs[index][j] = chrs[index];
+        } else {
+            if (res[j].ptr && res[j].checksum != checksum(res[j].ptr, res[j].size)) {
+                printf("Checksum failed - 1");
+                return -1;
             }
-        }
-    }
 
-    /* Make sure our data is still intact */
-    for (int i = 0; i < ITERATION_COUNT; ++i) {
-        for (int j = 0; j < sizes[i]; ++j) {
-            if (ptrs[i][j] != chrs[i]) {
-              return -1;
+            if (rand() & 1) {
+                size_t sz = RAND_SIZE();
+                void *x = mem_realloc(res[j].ptr, sz);
+
+                if (x != NULL) {
+                    res[j].ptr = x;
+                    res[j].size = sz;
+                    fill(res[j].ptr, sz);
+                    res[j].checksum = checksum(res[j].ptr, sz);
+                }
+            } else {
+                mem_free(res[j].ptr);
+                res[j].ptr = NULL;
+                res[j].size = 0u;
+                res[j].checksum = 0u;
             }
+
+            mem_show(res[j].ptr);
         }
     }
 
     /* Clean up our mess */
-    for (int i = 0; i < ITERATION_COUNT; ++i) {
-        _free(ptrs[i], (size_t) sizes[i]);
-        ptrs[i] = NULL;
-        sizes[i] = 0;
-        chrs[i] = '\0';
+    for (int i = 0; i < N; ++i) {
+        if (res[i].ptr && res[i].checksum != checksum(res[i].ptr, res[i].size)) {
+            printf("Checksum failed - 2");
+            return -1;
+        }
+        _free(res[i].ptr, (size_t) res[i].size);
     }
 
     printf("PASS!");
